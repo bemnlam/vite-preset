6ef1c8365a98f3fabd9c66429191c5d93880162f
Ben Lam <ben.lam@haymarket.asia>
2023-08-06 22:08:15 -0500

    feat: add App and Nginx config files

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <p>Vite running at <a href="http://localhost:5173">https://localhost:5173</a></p>
+        <p>Serving SSL with Nginx at <a href="https://localhost:3001">https://localhost:3001</a></p>
+        <p>See Nginx conf at <a href="/local-ssl.conf">{window.location.href}local-ssl.conf</a></p>
+      </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
